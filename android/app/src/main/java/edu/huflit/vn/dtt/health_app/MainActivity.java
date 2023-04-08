7d36280672eca84f8c833f76cf1d703a417ef983
dttaivlo1 <dttaivlo@gmail.com>
2023-04-08 13:10:12 +0700

    init

---
+++ android/app/src/main/java/edu/huflit/vn/dtt/health_app/MainActivity.java
@@ -0,0 +1,6 @@
+package edu.huflit.vn.dtt.health_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/Dashboard/hotel_home_screen.dart
@@ -0,0 +1,175 @@
+import 'package:health_app/Dashboard/hotel_list_view.dart';
+import 'package:health_app/Dashboard/model/hotel_list_data.dart';
+import 'package:flutter/material.dart';
+import 'hotel_app_theme.dart';
+
+class HotelHomeScreen extends StatefulWidget {
+  @override
+  _HotelHomeScreenState createState() => _HotelHomeScreenState();
+}
+
+class _HotelHomeScreenState extends State<HotelHomeScreen>
+    with TickerProviderStateMixin {
+  AnimationController? animationController;
+  List<HotelListData> hotelList = HotelListData.hotelList;
+  final ScrollController _scrollController = ScrollController();
+
+  DateTime startDate = DateTime.now();
+  DateTime endDate = DateTime.now().add(const Duration(days: 5));
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+        duration: const Duration(milliseconds: 1000), vsync: this);
+    super.initState();
+  }
+
+
+
+  @override
+  void dispose() {
+    animationController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: HotelAppTheme.buildLightTheme(),
+      child: Container(
+        child: Scaffold(
+          body: Stack(
+            children: <Widget>[
+              InkWell(
+                splashColor: Colors.transparent,
+                focusColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                hoverColor: Colors.transparent,
+                onTap: () {
+                  FocusScope.of(context).requestFocus(FocusNode());
+                },
+                child: Column(
+                  children: <Widget>[
+
+                    Expanded(
+                      child: NestedScrollView(
+                        controller: _scrollController,
+                        headerSliverBuilder:
+                            (BuildContext context, bool innerBoxIsScrolled) {
+                          return <Widget>[
+                            SliverList(
+                              delegate: SliverChildBuilderDelegate(
+                                  (BuildContext context, int index) {
+                                return Column(
+                                  children: <Widget>[
+
+                                  ],
+                                );
+                              }, childCount: 1),
+                            ),
+
+                          ];
+                        },
+                        body: Container(
+                          color:
+                              HotelAppTheme.buildLightTheme().backgroundColor,
+                          child: ListView.builder(
+                            itemCount: hotelList.length,
+                            padding: const EdgeInsets.only(top: 8),
+                            scrollDirection: Axis.vertical,
+                            itemBuilder: (BuildContext context, int index) {
+                              final int count =
+                                  hotelList.length > 10 ? 10 : hotelList.length;
+                              final Animation<double> animation =
+                                  Tween<double>(begin: 0.0, end: 1.0).animate(
+                                      CurvedAnimation(
+                                          parent: animationController!,
+                                          curve: Interval(
+                                              (1 / count) * index, 1.0,
+                                              curve: Curves.fastOutSlowIn)));
+                              animationController?.forward();
+                              return HotelListView(
+                                callback: () {},
+                                hotelData: hotelList[index],
+                                animation: animation,
+                                animationController: animationController!,
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget getListUI() {
+    return Container(
+      decoration: BoxDecoration(
+        color: HotelAppTheme.buildLightTheme().backgroundColor,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              offset: const Offset(0, -2),
+              blurRadius: 8.0),
+        ],
+      ),
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height - 156 - 50,
+            child: FutureBuilder<bool>(
+              builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+                if (!snapshot.hasData) {
+                  return const SizedBox();
+                } else {
+                  return ListView.builder(
+                    itemCount: hotelList.length,
+                    scrollDirection: Axis.vertical,
+                    itemBuilder: (BuildContext context, int index) {
+                      final int count =
+                          hotelList.length > 10 ? 10 : hotelList.length;
+                      final Animation<double> animation =
+                          Tween<double>(begin: 0.0, end: 1.0).animate(
+                              CurvedAnimation(
+                                  parent: animationController!,
+                                  curve: Interval((1 / count) * index, 1.0,
+                                      curve: Curves.fastOutSlowIn)));
+                      animationController?.forward();
+
+                      return HotelListView(
+                        callback: () {},
+                        hotelData: hotelList[index],
+                        animation: animation,
+                        animationController: animationController!,
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+ 
+
+  @override
+  double get maxExtent => 52.0;
+
+  @override
+  double get minExtent => 52.0;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
+  }
+}
